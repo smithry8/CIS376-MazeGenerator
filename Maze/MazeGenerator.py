c1664bf9c315daffab26e896af9008338355729c
smithry8 <smithry8@mail.gvsu.edu>
2023-02-04 17:14:02 -0500

    cant go off screen, random color

--- Maze/MazeGenerator.py
+++ Maze/MazeGenerator.py
@@ -19,6 +19,7 @@ playerRadius = tileSize/2
 player = Player.Player(playerRadius, playerRadius)
 walls = []
 stable = False
+rand = random.randrange(256)
 
 # The game Engine
 class Engine:
@@ -39,19 +40,19 @@ class Engine:
                 #Handle the Movement of the player
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                        if collisionDetector(player.x + tileSize, player.y)[1] == False:
+                        if collisionDetector(player.x + tileSize, player.y)[1] == False and player.x + tileSize < DISPLAYSURF.get_width():
                             player.x += tileSize
                             player.position = (player.x,player.y)
                     elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                        if collisionDetector(player.x - tileSize, player.y)[1] == False:
+                        if collisionDetector(player.x - tileSize, player.y)[1] == False and player.x - tileSize >= 0:
                             player.x -= tileSize
                             player.position = (player.x,player.y)
                     elif event.key == pygame.K_UP or event.key == pygame.K_w:
-                        if collisionDetector(player.x, player.y - tileSize)[1] == False:
+                        if collisionDetector(player.x, player.y - tileSize)[1] == False and player.y - tileSize >= 0:
                             player.y -= tileSize
                             player.position = (player.x,player.y)
                     elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                        if collisionDetector(player.x, player.y + tileSize)[1] == False:
+                        if collisionDetector(player.x, player.y + tileSize)[1] == False and player.y + tileSize < DISPLAYSURF.get_height():
                             player.y += tileSize
                             player.position = (player.x,player.y)
                     # used to check if the player has reached the bottom right tile for win
@@ -106,7 +107,7 @@ def drawWalls():
     for i in range(gridSize):
         for j in range(gridSize):
            if(grid[i][j] == "W"):
-                pygame.draw.rect(DISPLAYSURF, (255, 0, 0), (i * tileSize, j * tileSize, tileSize, tileSize))
+                pygame.draw.rect(DISPLAYSURF, (255, 0, rand), (i * tileSize, j * tileSize, tileSize, tileSize))
 # detects if a point collides with a Wall
 def collisionDetector(x,y):
     for w in walls:
