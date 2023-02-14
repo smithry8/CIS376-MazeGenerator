63f0586c4e23aa27abd24e735af6d22285f83dc1
IanO12 <ostrandi@mail.gvsu.edu>
2023-02-13 21:10:23 -0500

    Reorganized

--- Maze/Drawable.py
+++
@@ -1,8 +0,0 @@
-# Created by Ian # Created by Ian
-from abc import ABC, abstractmethod
-
-# Interface for drawable objects
-class Drawable(ABC):
-    @abstractmethod
-    def Draw(self):
-        pass
--- Maze/Updateable.py
+++
@@ -1,7 +0,0 @@
-# Created by Ian
-from abc import ABC, abstractmethod
-# interface for updateable objects
-class Updateable(ABC):
-    @abstractmethod
-    def Update(self):
-        pass
