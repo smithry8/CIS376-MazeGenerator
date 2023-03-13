ace952f0c29c643c394392e72f517dddf0d56584
IanO12 <ostrandi@mail.gvsu.edu>
2023-03-13 12:07:13 -0400

    Midterm project start

--- GameEngine/Drawable.py
+++ GameEngine/Drawable.py
@@ -1,6 +1,6 @@
 # Created by Ian # Created by Ian
 from abc import ABC, abstractmethod
-
+import pygame as pg
 # Interface for drawable objects
 class Drawable(ABC):
     @abstractmethod
--- GameEngine/GameObject.py
+++ GameEngine/GameObject.py
@@ -1,33 +1,35 @@
 # Created by Ian
 from GameEngine import Updateable
 from GameEngine import Drawable
-
+import pygame as pg
 # Object that holds all gameobject information
 class GameObject():
-    def __init__(self,x,y,w,h,layer,tag,collidable):
+    def __init__(self,x,y,w,h,tag,collidable):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.y = y
         self.tag = tag
-        self.layer = layer
         self.collidable = collidable
 
 # Game object that is drawable
-class DGameObject(GameObject, Drawable.Drawable):
-    def __init__(self, x, y, w, h, layer, tag, collidable):
-        GameObject.__init__(self, x, y, w, h, layer, tag, collidable)
+class DGameObject(GameObject, Drawable.Drawable, pg.sprite.Sprite):
+
+    def __init__(self, x, y, w, h, tag, collidable):
+        GameObject.__init__(self, x, y, w, h, tag, collidable)
+        pg.sprite.Sprite.__init__(self)
+        self.layer = 0
 
 # Game object that is updateable
 class UGameObject(GameObject,Updateable.Updateable):
-    def __init__(self, x, y, w, h, layer, tag, collidable):
-        GameObject.__init__(self, x, y, w, h, layer, tag, collidable)
+    def __init__(self, x, y, w, h, tag, collidable):
+        GameObject.__init__(self, x, y, w, h, tag, collidable)
 
     def Update(self):
         pass
 
 # Game object that is drawable and updateable
 class DUGameObject(DGameObject, Updateable.Updateable):
-    def __init__(self, x, y, w, h, layer, tag, collidable):
-        DGameObject.__init__(self, x, y, w, h, layer, tag, collidable)
+    def __init__(self, x, y, w, h, tag, collidable):
+        DGameObject.__init__(self, x, y, w, h, tag, collidable)
